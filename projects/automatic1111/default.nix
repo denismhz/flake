b3914b1459cd1ab45d3ce73594e4fe2266ae52bf
Denis Manherz <denis@manherz.de>
2023-11-27 13:59:33 +0100

    Update

--- projects/automatic1111/default.nix
+++ projects/automatic1111/default.nix
@@ -90,7 +90,7 @@ in
     in
     {
       a1111 = ./nixos;
-      invokeai-nvidia = {
+      a1111-nvidia = {
         imports = [
           config.flake.nixosModules.invokeai
           (packageModule "a1111-nvidia")
